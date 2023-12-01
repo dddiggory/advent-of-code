017282c221b70778c97ce41434f84b3e9558f797
diggory-rycroft <diggory.rycroft@vercel.com>
2023-12-01 11:27:21 -0500

    part 1

--- 2023/1.js
+++ 2023/1.js
@@ -1,28 +1,37 @@
-let sampleInput = `
-    1abc2
-    pqr3stu8vwx
-    a1b2c3d4e5f
-    treb7uchet
-    `
+const fs = require('fs');
+const filePath = './inputs/1.txt';
 
-let calibrationValues = []
-
-sampleInput.split('\n').forEach( (line) => {
-    let calibrationValue, digit1, digit2
-
-    Array.from(line).forEach( (character) => {
-
-        if (character!=" " && !isNaN(Number(character))) {
-            if (!digit1) {
-                digit1 = character;
-            }
-            digit2 = character;
-        } 
-    })
-    calibrationValue = Number(digit1+digit2)
-    console.log(calibrationValue)
-    if (!isNaN(calibrationValue)) {
-        calibrationValues.push(calibrationValue);
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
     }
     
-})
+    console.log(sumCalibrationValues(data));
+  });
+
+const sumCalibrationValues = (input) => {
+
+    let runningTotal = 0
+    
+    input.split('\n').forEach( (line) => {
+        let calibrationValue, digit1, digit2
+    
+        Array.from(line).forEach( (character) => {
+    
+            if (character!=" " && !isNaN(Number(character))) {
+                if (!digit1) {
+                    digit1 = character;
+                }
+                digit2 = character;
+            } 
+        })
+        calibrationValue = Number(digit1+digit2)
+        if (!isNaN(calibrationValue)) {
+            runningTotal += calibrationValue
+        }
+    })
+
+    return runningTotal;
+
+}
