cbe7bbc4e9b3212a9abd6e729ce4fc2107f65cf5
diggory-rycroft <diggory.rycroft@vercel.com>
2023-12-05 10:04:24 -0500

    day 5 part 1 cleanup

--- 2023/5.js
+++ 2023/5.js
@@ -15,10 +15,9 @@ const mainFunction = (input) => {
     let seeds = []
 
     for (let m in maps) { // for each map
-        let desc = maps[m].split(':')[0]
+        let desc = maps[m].split(':')[0] //just for logging
         let mapStrings = maps[m].split(':')[1].split('\n').filter((value) => value)
         let mapRules = []
-        let newSeeds = []
 
         if (m==0) { //seeds
             seeds = mapStrings[0].split(' ').filter((value) => value)
@@ -49,21 +48,7 @@ const mainFunction = (input) => {
 
         }
         
-        // console.log({mapRules})
-        // console.log({desc}, {seeds})
-
-        // if (m>=1) {process.exit()}
-        
-        //establish ranges & transform logic
-        // console.log({desc}, mapRules)
-
-        //for each seed
-        //with each map, transform it into the next number
-        //no need to store the originals
-        //array
-        
     }
-    // console.log(maps[0])
 
     return Math.min(...seeds)
 }
